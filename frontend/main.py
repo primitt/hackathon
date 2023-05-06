ada89143e3f49421393f43ad0ab85dd80d5cad18
primboi <81822069+primitt@users.noreply.github.com>
2023-05-06 10:41:09 -0700

    Add HTML shit

--- frontend/main.py
+++ frontend/main.py
@@ -2,3 +2,10 @@ from flask import Flask, request, render_template
 import requests
 import os
 import sys
+import hashlib 
+
+app = Flask(__name__)
+
+@app.route("/", method=["GET"])
+def index():
+    return render_template("index.html")
