19b34e135d3dc4f1237f5fee61e9faf08c88c309
HeckerWithAHat <aryanj1010@gmail.com>
2023-05-06 15:12:03 -0700

    recipe requets

--- backend/src/main/kotlin/dev/primitt/Application.kt
+++ backend/src/main/kotlin/dev/primitt/Application.kt
@@ -4,11 +4,15 @@ import com.expediagroup.graphql.client.spring.GraphQLWebClient
 import dev.primitt.plugins.*
 import io.ktor.server.application.*
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.Database
+    import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.HttpURLConnection
+import java.net.URI
 import java.net.URL
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 
 fun main(args: Array<String>): Unit =
@@ -24,11 +28,13 @@ fun Application.module() {
     transaction {
         SchemaUtils.create(Users)
     }
-    val client = GraphQLWebClient(url = "https://production.suggestic.com/graphql")
-    runBlocking {
-        val helloWorldQuery = HelloWorldQuery()
-        val result = client.execute(helloWorldQuery)
-        println("hello world query result: ${result.data?.helloWorld}")
-    }
+}
+fun complexSearch(query: String, diet: String, intolerances: String){
+    val client = HttpClient.newBuilder().build();
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://api.spoonacular.com/recipes/complexSearch?apiKey=5a5bb29a98ef4762917c9e17af5553f2&query=$query&diet=$diet&intolerances=$intolerances"))
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 }
